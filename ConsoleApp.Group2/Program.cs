49b1dc07565a228a109fe89eb173c5bdbcadca83
wendy <wendy@MHZCOMPLICATED>
2023-02-18 19:31:28 -0500

    Add project files.

---
+++ ConsoleApp.Group2/Program.cs
@@ -0,0 +1,82 @@
+﻿// print to screen
+            Console.WriteLine("Hello, World!");
+
+//Input and variables. Same rules apply, it is very case sensitie
+string fname = string.Empty;
+string lname = "";
+int age = 0;
+char gender = '';
+bool employed = false;
+double salary = 0;
+decimal pay;
+decimal tax;
+decimal taxRate;
+
+
+DateTime dob;
+DateOnly employedDate;
+TimeOnly WakingTime;
+
+Console.WriteLine("Enter your name");
+fname= Console.ReadLine();
+Console.WriteLine(fname);
+
+Console.Write("Enter your age");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(age);
+tax = 
+Console.WriteLine(gender);
+
+//three types of construct: sequence(Write a program in the order of the operation), condition(if statements 
+//  switch statement, etc) and repition
+
+if (age > 1)
+{
+
+}else if (age < 10)
+{
+
+}
+else
+{
+
+}
+
+switch (salary)
+{
+    case 100:
+        Console.WriteLine("");
+        break;
+    default:
+        break;
+}
+while(employed == true)
+
+{
+
+}
+do
+{
+
+} while (employed == true);
+
+for (int i = 0; i < 12; i++)
+{
+
+}
+
+int[] grades = new int[20];
+int[] ages = {10, 20,30,40,50};
+
+//collection types- arrays, lists, IEnumerable, IQuertable, IObservable
+var students = new List<string>();
+students.Add("Richardo)";
+
+students.Remove("Richardo");
+var stuname = students[0];
+
+foreach (var in students)
+{
+
+}
